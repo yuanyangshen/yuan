ea94f650e892684b699f7c95ec1d81d265c66165
shenyy <shenyy@fiberhome.com>
2018-07-05 17:08:38 +0800

    add shiro

---
+++ src/main/java/com/shenyy/yuan/common/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.shenyy.yuan.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        if(!registry.hasMappingForPattern("/static/**")){
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
+        super.addResourceHandlers(registry);
+    }
+}
